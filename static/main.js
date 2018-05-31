02ff2b530646b1b6820bdbc71f9aa7ffc174df0f
lee-aaron <lee.aaron.68@gmail.com>
2018-05-30 18:26:05 -0700

    Added research links; Will incorporate content later and works cited

--- static/main.js
+++ static/main.js
@@ -11,8 +11,8 @@ var text1 = document.createElement("p");
 text1.style.fontFamily = "Architects Daughter";
 text1.style.fontSize = "17px";
 text1.textContent = "Looking at this graph, we see that 31% of the food supply went uneaten in the US in 2010. Yet, these excess foods cost a lot especially for farmers. \
-                    Today, it is crucial to preserve our environment and farmers can play their role by minimizing the amount of extra food made. \
-                    In order to help farmers reduce the amount of excessive food produced, we can look at two simple solutions! Scroll down to learn more.";
+                    Today, it is crucial to preserve our environment and there are ways farmers can reduce the impact of excess food. \
+                    In order to help farmers, we can look at two simple solutions! Scroll down to learn more.";
 
 /* Sets the elements within the intro div */
 document.getElementById("intro").appendChild(intro);
@@ -27,13 +27,13 @@ document.getElementById("left-panel").addEventListener('click', function() {
     document.getElementById('left-panel').classList.toggle('expand');
 
     var rightPanel = document.getElementById('right-panel');
-    /* smoothly inserts right solution back after 0.9s */
+    /* smoothly inserts right solution back after 1.15s */
     if (rightPanel.style.display === 'none') {
         setTimeout(function (){
         
             rightPanel.style.display = rightPanel.style.display === 'none' ? '' : 'none';
           
-        }, 900);
+        }, 1150);
     } else {
         rightPanel.style.display = rightPanel.style.display === 'none' ? '' : 'none';
     }
@@ -43,13 +43,13 @@ document.getElementById("right-panel").addEventListener('click', function() {
     document.getElementById('right-panel').classList.toggle('expand');
 
     var leftPanel = document.getElementById('left-panel');
-    /* smoothly inserts left solution back after 0.9s */
+    /* smoothly inserts left solution back after 1.15s */
     if (leftPanel.style.display === 'none') {
         setTimeout(function (){
         
             leftPanel.style.display = leftPanel.style.display === 'none' ? '' : 'none';
           
-        }, 900);
+        }, 1150);
     } else {
         leftPanel.style.display = leftPanel.style.display === 'none' ? '' : 'none';
     }
